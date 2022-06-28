7fb6dff80ac154c68a345e8f72be1f7a66521023
Wade Graves <wadegraves940@gmail.com>
2022-06-27 20:51:13 -0400

    added my questions

---
+++ app.js
@@ -0,0 +1,28 @@
+var categories = ['Sports', 'Programming', 'Quick Maths', 'Disney/Pixar'];
+var questions = [
+    [ // Sports
+        'This team won the 2020 Nba Championship.', // The Milwaukee Bucks
+        'This NBA Player and Coach has 11 NBA Titles, the most ever.', // Bill Russell
+        'This football team just won the SuperBowl.', //  LA Rams
+        'The Masters, "A Tradition Unlike Any Other," is played at this golf course.', //  Augusta National
+    ],
+    [ // States
+        'This State is home to the Space Needle', // Washington
+        'This state is home to a famous strip', // Nevada
+        'This state is famous for the cheese and beer', // Wisconsin
+        'They say everything is bigger in this state', //Texas
+        
+    ],
+    [ // Quick Maths
+        '10x9', //  90
+        'This shape has 8 sides.', // an Octagon?
+        'These are the first 5 prime numbers.', //   2, 3, 5, 7, 11
+        '2 to the 8th power' //  256
+    ],
+    [ // Disney/Pixar
+        'Even miracles take a little time.', // Fairy Godmother from Cinderella?
+        'Ohana means family. Family means no one gets left behind.', //  Lilo from Lilo and Stitch?
+        'You\'re braver than you believe, and stronger than you seem, and smarter than you think.', //  Winnie the Pooh?
+        'The problem is not the problem. The problem is your attitude about the problem.', //  is Jack Sparrow from Pirates of the Caribbean?
+    ]
+];
